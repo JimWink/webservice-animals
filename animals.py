078474f3dde827ede8218756e3d5fb767da7337d
Eric Appelt <eric.appelt@gmail.com>
2017-04-02 16:50:04 -0500

    Add auth framework and decorator

--- animals.py
+++ animals.py
@@ -1,6 +1,9 @@
 from sanic import Sanic
 from sanic.response import text
 
+from base64 import b64decode
+from functools import wraps
+
 from asyncio import sleep, Lock
 import aioredis
 
@@ -37,6 +40,32 @@ app = Sanic(__name__)
 rp = RedisPoolSingleton()
 
 
+async def check_auth(username, password):
+    return username == 'foo' and password == 'bar'
+
+
+async def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return text(
+        'Incorrect credentials with Basic Auth.',
+        status=401, 
+        headers={'WWW-Authenticate': 'Basic realm="Login Required"'}
+    )
+
+
+def requires_auth(f):
+    @wraps(f)
+    async def decorated(request, *args, **kwargs):
+        try:
+            auth = b64decode(request.token).decode().split(':')
+        except Exception as e:
+            return await authenticate()
+        if not await check_auth(auth[0], auth[1]):
+            return await authenticate()
+        return await f(request, *args, **kwargs)
+    return decorated
+
+
 @app.route('/animals/')
 async def hello(request):
     """
@@ -59,11 +88,13 @@ async def speak(request, animal):
         print(e)
         return text('The animal {0} was not found.'.format(animal), status=404)
     
+    print(b64decode(request.token).decode().split(':'))
     await sleep(5)
     return text(val.decode(), status=200)
 
 
 @app.route('/animals/<animal>', methods=['PUT'])
+@requires_auth
 async def add_animal(request, animal):
     """
     Add an animal to the database
